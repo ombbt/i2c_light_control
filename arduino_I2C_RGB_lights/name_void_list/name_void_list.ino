8fbfedef6c7629d72efb520c9b421d4f18dad591
ombbt <113554625+ombbt@users.noreply.github.com>
2023-01-26 09:28:49 +0100

    Create name_void_list.ino

---
+++ arduino_I2C_RGB_lights/name_void_list/name_void_list.ino
@@ -0,0 +1,133 @@
+unsigned long name_strombo_timmer;  
+int name_strombo_timeset;
+int name_strombo_status;
+void name_strombo(int oncolor[], int offcolor[], unsigned long ontime, unsigned long offtime) {  
+
+unsigned long name_backgroundstrombo_timmer;
+int name_backgroundstrombo_timeset;
+int name_backgroundstrombo_status;
+void name_backgroundstrombo(int oncolor[], unsigned long ontime, unsigned long offtime) {
+
+unsigned long name_someblack_timmer;  
+int name_someblack_timeset;
+int name_someblack_status;
+int name_someblack_activate;
+void name_someblack(int oncolor[], unsigned long ontime, unsigned long offtime) {
+
+unsigned long name_somemulticolors_timmer;  
+int name_somemulticolors_timeset;
+int name_somemulticolors_status;
+int name_somemulticolors_activate;
+int name_somemulticolors_active;
+void name_somemulticolors(int color1[], int color2[], int color3[], int color4[], unsigned long time1, unsigned long time2, unsigned long time3, unsigned long time4, unsigned long donothingtime) {
+
+
+unsigned long name_colors3_timmer;   
+int name_colors3_timeset;
+int name_colors3_status;
+void name_colors3(int color1[], int color2[], int color3[], unsigned long time1, unsigned long time2, unsigned long time3) {
+
+ 
+unsigned long name_colors8_timmer;  
+int name_colors8_timeset;
+int name_colors8_status;
+void name_colors8(int color1[], int color2[], int color3[], int color4[], int color5[], int color6[], int color7[], int color8[], unsigned long time1, unsigned long time2, unsigned long time3, unsigned long time4, unsigned long time5, unsigned long time6, unsigned long time7, unsigned long time8) {
+
+ 
+int name_fadesiren_status;
+int name_fadesiren_timeset;
+unsigned long name_fadesiren_timmer1;
+unsigned long name_fadesiren_timmer2;
+void name_fadesiren(int startcolor[], int endcolor[], unsigned long finaltime) {
+
+void name_fixedcolors(int color1[]) {
+
+int name_fade1_timeset;
+int name_fade1_offsetset;
+int name_fade1_init;
+void name_fade1(int startcolor[], int endcolor[], unsigned long totaltime, int offsetlist[]) {
+
+
+int name_fade3_timeset;
+int name_fade3_offsetset;
+int name_fade3_init;
+
+void name_fade3(int color1[], int color2[], int color3[], unsigned long totaltime, int offsetlist[]) {
+
+void name_fixed1(int color1[], int color2[], int color3[], int color4[], int color5[], int color6[], int color7[], int color8[], int color9[], int color10[], int color11[], int color12[], int color13[], int color14[], int color15[], int color16[], int color17[],int color18[],int color19[], int color20[], int color21[], int color22[]){
+
+void name_THEfixedcolors(int Tcolor[], int Hcolor[], int Ecolor[]) {
+
+void name_ONEfixedcolors(int Ocolor[], int Ncolor[], int Ecolor[]) {
+
+void name_MANfixedcolors(int Mcolor[], int Acolor[], int Ncolor[]) {
+
+void name_BIGfixedcolors(int Bcolor[], int Icolor[], int Gcolor[]) {
+
+void name_BANDfixedcolors(int Bcolor[], int Acolor[], int Ncolor[], int Dcolor[]) {
+
+void name_THEORYfixedcolors(int Tcolor[], int Hcolor[], int Ecolor[], int Ocolor[], int Rcolor[], int Ycolor[]) {
+
+unsigned long name_THEblink_timmer; //idem THE ONE MAN BIG
+int name_THEblink_timeset;
+int name_THEblink_status;
+int name_THEblink_loopcount;
+void name_THEblink(int T1color[], int H1color[], int E1color[], unsigned long blacktime, unsigned long colortime){
+
+unsigned long name_BANDblink_timmer;   
+int name_BANDblink_timeset;
+int name_BANDblink_status;
+int name_BANDblink_loopcount;
+void name_BANDblink(int B5color[], int A5color[], int N5color[], int D5color[], unsigned long blacktime, unsigned long colortime){
+
+unsigned long name_THEORYblink_timmer;   
+int name_THEORYblink_timeset;
+int name_THEORYblink_status;
+int name_THEORYblink_loopcount;
+void name_THEORYblink(int T6color[], int H6color[], int E6color[], int O6color[], int R6color[], int Y6color[], unsigned long blacktime, unsigned long colortime){
+}
+
+unsigned long name_THEchenillar1_timmer;  //idem THE ONE MAN BIG chennillar 1 2 3 disponible
+int name_THEchenillar1_timeset;
+int name_THEchenillar1_status;
+int name_THEchenillar1_loopcount;
+void name_THEchenillar1(int T1color[], int H1color[], int E1color[], unsigned long steptime){
+
+unsigned long name_BANDchenillar2_timmer;   //chenillar 1 2 3 disponible
+int name_BANDchenillar2_timeset;
+int name_BANDchenillar2_status;
+int name_BANDchenillar2_loopcount;
+void name_BANDchenillar2(int B5color[], int A5color[], int N5color[], int D5color[], unsigned long steptime){
+
+unsigned long name_THEORYchenillar1_timmer;   //chenillar 1 2 3 4 5 dispo
+int name_THEORYchenillar1_timeset;
+int name_THEORYchenillar1_status;
+int name_THEORYchenillar1_loopcount;
+void name_THEORYchenillar1(int T6color[], int H6color[], int E6color[], int O6color[], int R6color[], int Y6color[], unsigned long steptime){
+
+unsigned long name_chenillarcolor1_timmer;   
+int name_chenillarcolor1_timeset;
+int name_chenillarcolor1_status;
+int name_chenillarcolor1_loopcount;
+void name_chenillarcolor1(int color1[], unsigned long steptime){
+
+unsigned long name_chenillar3color_timmer;   
+int name_chenillar3color_timeset;
+int name_chenillar3color_status;
+int name_chenillar3color_loopcount;
+void name_chenillar3color(int color1[], int color2[], int color3[], unsigned long steptime){
+
+  
+}
+}
+
+}
+
+
+
+
+
+
+
+  
+}
